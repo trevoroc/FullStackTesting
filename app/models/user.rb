6d5a3508ffba506cff6883defc2bbfecc80f2f9f
App Academy Student <student@appacademy.io>
2017-04-21 15:09:40 -0700

    initial user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,31 @@
 #
 
 class User < ActiveRecord::Base
+  validates :user_name, :password_digest, :session_token, presence: true
+  validates :password, length: { minimum: 6 }, allow_nil: true
+  validates :user_name, uniqueness: true
+
+  attr_reader :password
+  after_initialize :ensure_session_token
+
+  def self.find_by_credentials(user_name, password)
+    user = User.find_by(user_name: user_name)
+    return nil if user.nil?
+    user.is_password?(password) ? user : nil
+  end
+
+  def self.generate_session_token
+    SecureRandom::urlsafe_base64
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  private
+
+  def ensure_session_token
+    self.session_token ||= self.class.generate_session_token
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -43,7 +43,7 @@ RSpec.describe User, type: :model do
     it { should validate_presence_of(:password_digest) }
     it { should validate_presence_of(:session_token) }
     it { should validate_length_of(:password).
-        is_at_least(10).
+        is_at_least(6).
         on(:save) }
     it { should validate_uniqueness_of(:user_name) }
   end
