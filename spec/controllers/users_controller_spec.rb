f66d8ac3aa04919a7afde9af42b75d6c2af23ecf
App Academy Student <student@appacademy.io>
2017-04-21 14:42:01 -0700

    Begin UsersController spec

---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+RSpec.describe UsersController, type: :controller do
+  describe "GET #new" do
+    it "renders the new template" do
+      get :new, {}
+      expect(response).to render_template("new")
+    end
+  end
+
+  describe "POST #create" do
+    context "with invalid params" do
+      it "validates the presence of the user's email and password" do
+        post :create, {user_name: 'joe'}
+        expect(flash[:errors]).to_not be_nil
+        expect(response).to render_template("new")
+      end
+      it "validates that the password is at least 6 characters long" do
+        post :create, {user_name: 'joe', password: 'abcd'}
+        expect(flash[:errors]).to_not be_nil
+        expect(response).to render_template("new")
+      end
+    end
+    context "with valid params" do
+      it "redirects user to the current user\'s showpage" do
+        post :create, { user_name: 'joe', password: 'abcdef' }
+        it { should redirect_to(user_url(User.last)) }
+        it { should redirect_to(action: :show) }
+      end
+    end
+  end
+end
