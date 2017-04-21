8398d1b8b36bd0d00e3a3e7611d93dcd52470a92
App Academy Student <student@appacademy.io>
2017-04-21 15:59:10 -0700

    Initial Capy specs

---
+++ spec/features/auth_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+require 'spec_helper'
+
+feature "the signup process" do
+
+  scenario "has a new user page"
+
+  feature "signing up a user" do
+
+    scenario "shows username on the homepage after signup"
+
+  end
+
+end
+
+feature "logging in" do
+
+  scenario "begins with a logged out state"
+
+  scenario "doesn't show username on the homepage after logout"
+  
+end
